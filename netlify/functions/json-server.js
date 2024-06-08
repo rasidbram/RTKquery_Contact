c2afc9c8000ab7c37c5f9fefbc208f488fe51897
rasid bram <rashidaynur.ibram@yandex.com>
2024-06-08 13:50:58 +0100

    debugging

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -4,8 +4,14 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
+
+app.use((req, res, next) => {
+	console.log(`Received request: ${req.method} ${req.path}`);
+	next();
+});
 
 app.use(middlewares);
 app.use(router);
